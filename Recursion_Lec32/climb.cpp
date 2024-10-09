78f63e2913b18f3aa512fa84fb70818a1cb65404
Prabhjeet <prabhjeetsandhu010@gmail.com>
2024-10-09 17:26:35 +0530

    climb problem insertion

---
+++ Recursion_Lec32/climb.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int climb(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    if (n < 0)
+    {
+        return 0;
+    }
+    return climb(n - 1) + climb(n - 2);
+}
+int main()
+{
+    cout << "Enter N: ";
+    int n;
+    cin >> n;
+    cout << climb(n);
+    return 0;
+}

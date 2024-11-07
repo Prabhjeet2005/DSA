7644c6d217b46124c69b4802971393aaf1887a26
Prabhjeet <prabhjeetsandhu010@gmail.com>
2024-11-08 03:40:30 +0530

    Queue Complete

--- Lec61_QueueQuesALL/MaxMinSumSubarray.cpp
+++ Lec61_QueueQuesALL/MaxMinSumSubarray.cpp
@@ -9,7 +9,8 @@ int sum(int *arr, int k, int n)
 
     for (int i = 0; i < k; i++)
     {
-        while(!maxq.empty() && arr[maxq.front()]<=arr[i]){
+        while (!maxq.empty() && arr[maxq.front()] <= arr[i])
+        {
             maxq.pop_front();
         }
         while (!minq.empty() && arr[minq.front()] >= arr[i])
@@ -61,3 +62,25 @@ int main()
     cout << sum(arr, k, n) << endl;
 }
 
+// BRUTE FORCE TC => O(n*k)  SC=>O(1)
+
+// int main()
+// {
+//     int arr[7] = {2, 5, -1, 7, -3, -1, -2};
+//     int k = 4;
+//     int sum = 0;
+
+//     for (int i = 0; i <= 7 - k; i++)
+//     {
+//         int mini = INT_MAX;
+//         int maxi = INT_MIN;
+//         for (int j = i; j < i + k; j++)
+//         {
+//             maxi = max(arr[j], maxi);
+//             mini = min(arr[j], mini);
+//         }
+//         sum += maxi + mini;
+//         cout << "MAX: " << maxi << ", MIN: " << mini << "SUM: " << sum << endl;
+//     }
+//     cout << "SUM: " << sum << endl;
+// }

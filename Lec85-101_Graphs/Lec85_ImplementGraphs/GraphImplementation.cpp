c59a8df418f06a126b99d360d02e9d1a0a64afcc
Prabhjeet <prabhjeetsandhu010@gmail.com>
2024-12-31 14:54:28 +0530

    Lec 85 Graph Implement

--- Lec85-101_Graphs/Lec85_ImplementGraphs/GraphImplementation.cpp
+++ Lec85-101_Graphs/Lec85_ImplementGraphs/GraphImplementation.cpp
@@ -8,24 +8,25 @@ class graph
 public:
   unordered_map<int, list<int>> adj;
 
+  //  direction true->Directed Graph
+  //  direction false->Undirected Graph
   void addEdge(int u, int v, bool direction)
   {
     adj[u].push_back(v);
+
     if (direction == false)
     {
       adj[v].push_back(u);
     }
-  };
-
+  }
   void printAdjList()
   {
-    cout << "Adjacency List:-\n";
     for (auto i : adj)
     {
       cout << i.first << " -> ";
       for (auto j : i.second)
       {
-        cout << j << " , ";
+        cout << j << ",";
       }
       cout << endl;
     }
@@ -36,16 +37,16 @@ int main()
 {
   graph g;
   int n, m;
-  cout << "Enter No. of Nodes & Edges: ";
-  cin >> n >> m;
-
-  for (int i = 0; i < m; i++)
-  {
+  cout << "Enter No. of Nodes";
+  cin >> n;
+  cout << "Enter No. of Edges: ";
+  cin >> m;
+  for (int i = 0; i<m; i++){
     int u, v;
-    cout << "Enter u & v: ";
+    cout << "Enter u and v: ";
     cin >> u >> v;
     g.addEdge(u, v, 0);
   }
-
+  cout << "Adjacency List:- \n";
   g.printAdjList();
 }

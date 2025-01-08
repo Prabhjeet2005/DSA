07315513a95571fd99b354718d2bbf16b2d6c596
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-01-08 12:40:58 +0530

    Lec97 Kruskal

--- Lec85-101_Graphs/Lec97_KruskalMST/KruskalMST.cpp
+++ Lec85-101_Graphs/Lec97_KruskalMST/KruskalMST.cpp
@@ -1,63 +1,60 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-void makeSet(vector<int> &parent, vector<int> &rank, int n)
+void makeSet(vector<int> &parent, int n)
 {
   for (int i = 0; i < n; i++)
   {
-    parent[i] = i; // Self Loop
-    rank[i] = 0;   // Initialize each rank = 0
+    parent[i] = i;
   }
 }
 
 int findParent(vector<int> &parent, int node)
 {
-  if (parent[node] == node) // If Self Loop Found Means Parent
+  if (parent[node] == node)
   {
     return node;
   }
-  return parent[node] = findParent(parent, parent[node]); // String Compression by parent[node]
+  return parent[node] = findParent(parent, parent[node]);
 }
 
-void unionSet(vector<int> &parent, vector<int> &rank, int u, int v)
+void setUnion(vector<int> &parent, vector<int> &rank, int u, int v)
 {
-  // Compare Ranks of Parent
   u = findParent(parent, u);
   v = findParent(parent, v);
+
   if (rank[u] < rank[v])
   {
     parent[u] = v;
     rank[v]++;
   }
-  else if (rank[v] < rank[u])
+  else if (rank[u] > rank[v])
   {
     parent[v] = u;
     rank[u]++;
   }
-  else // Rank is Equal
-  {
+  else{
     parent[u] = v;
     rank[v]++;
   }
 }
 
 bool cmp(vector<int> &a, vector<int> &b)
-{ // Custom Comparator As we need to find on what bases to sort edges array
+{
   return a[2] < b[2];
 }
 
-int minSpanningTree(vector<vector<int>> &edges, int n)
+int Mst(vector<vector<int>> &edges, int n)
 {
+  vector<int> parent(n, -1);
+  vector<int> rank(n, 0);
 
-  vector<int> parent(n);
-  vector<int> rank(n);
-
-  sort(edges.begin(), edges.end(), cmp); // On Basis of Weight with help of cmp
-
-  makeSet(parent, rank, n);
+  makeSet(parent, n);
 
   int totalWt = 0;
+  sort(edges.begin(), edges.end(), cmp);
 
   for (int i = 0; i < edges.size(); i++)
   {
@@ -67,8 +64,8 @@ int minSpanningTree(vector<vector<int>> &edges, int n)
 
     if (u != v)
     {
+      setUnion(parent, rank, u, v);
       totalWt += wt;
-      unionSet(parent, rank, u, v);
     }
   }
   return totalWt;
@@ -78,5 +75,5 @@ int main()
 {
   vector<vector<int>> edges = {{1, 4, 1}, {1, 2, 2}, {2, 3, 3}, {2, 4, 3}, {1, 5, 4}, {3, 4, 5}, {2, 6, 7}, {3, 6, 8}, {4, 5, 9}};
   int n = 6;
-  cout << "Ans: " << minSpanningTree(edges, n);
+  cout << "Weight of MST: " << Mst(edges, n) << endl;
 }
